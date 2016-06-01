93ca2778699fa4792a11f75501a4e2974c425bc5
ruanjiannanhai <ruanjiannanhai@126.com>
2016-06-01 18:01:18 +0800

    construct the project

---
+++ app/friend/main.js
@@ -0,0 +1,11 @@
+/**
+ * Created by lilei on 16-6-1.
+ */
+
+var React = require('react');
+var ReactDom = require('react-dom');
+
+ReactDom.render(
+    <div>hello world</div>
+    ,document.getElementById('page')
+);
